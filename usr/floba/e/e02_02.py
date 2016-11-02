3dd61d55053b599cc03d83dd85aa9d4f398f3e86
andreafb677 <andreafloresbarrero@gmail.com>
2016-11-02 13:45:53 +0100

    Update e02_02.py

---
+++ usr/floba/e/e02_02.py
@@ -0,0 +1,9 @@
+>>> vocales = ["2", "4", "7", "5", "8"]
+>>> vocales
+['2', '4', '7']
+>>> vocales = vocales + ["U"]
+>>> vocales
+['E', 'I', 'O', 'U']
+>>> vocales = ["A"] + vocales
+>>> vocales
+['A', 'E', 'I', 'O', 'U']
