239a20d5098e84ff525b7bd96fd981504204792b
mariavazquez1999 <vazquezbazomaria@gmail.com>
2016-11-02 13:43:59 +0100

    Update e02_03.py

--- usr/vabam/e/e02_03.py
+++ usr/vabam/e/e02_03.py
@@ -1,1 +1,6 @@
+print("Convertidor de grados Fahrenheit a grados Celsius")
+fahrenheit = float(input("Escriba una temperatura en grados Fahrenheit: "))
 
+celsius = (fahrenheit-32) / 1.8
+
+print(fahrenheit, "ºF son", round(celsius, 1), "ºC")
