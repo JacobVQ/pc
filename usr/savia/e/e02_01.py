74a82bcc337931fa20e5f59415e4eb1e6b6ac4c5
alvarosv1996 <alvzr96@gmail.com>
2016-11-02 13:15:18 +0100

    Update and rename e02_01.md to e02_01.py

--- usr/savia/e/e02_01.py
+++ usr/savia/e/e02_01.py
@@ -2,7 +2,7 @@
 
 - e02_01.py : escribe un programa en Python 3 que pida cinco números y que escriba su media aritmética.
 
-```print('progresion')
+print('progresion')
 
 numeros = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
@@ -10,4 +10,4 @@ for i in numeros:
     suma = suma + 1
     
 media = suma / 10
-print('La media es: ' + str (media))```
+print('La media es: ' + str (media))
