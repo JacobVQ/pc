2958ca30660b41a61737dbe2a4da9a8283de5913
jmcarrasco99 <jm.carrascomesa@gmail.com>
2016-11-02 13:24:21 +0100

    Update e02_01.py

--- usr/camej/e/e02_01.py
+++ usr/camej/e/e02_01.py
@@ -8,5 +8,5 @@ numero5 = float(input("Escriba un número: "))
 
 media = (numero1+numero2+numero3+numero4+numero5) / 5
 
-print("La media aritmética de", numero1, "y", numero2,  "y", numero3,  "y", numero4,  "y", numero5 "es", media)
+print("La media aritmética de", numero1, "y", numero2,  "y", numero3,  "y", numero4,  "y", numero5, "es", media)
 
