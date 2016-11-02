d03b77265059f1560de130c899b1ce10bb7d04d8
jmcarrasco99 <jm.carrascomesa@gmail.com>
2016-11-02 13:19:41 +0100

    Create e02_01.py

---
+++ usr/camej/e/e02_01.py
@@ -0,0 +1,12 @@
+
+print("Cálculo de la media de dos números")
+numero1 = float(input("Escriba un número: "))
+numero2 = float(input("Escriba otro número: "))
+numero3 = float(input("Escriba un número: "))
+numero4 = float(input("Escriba un número: "))
+numero5 = float(input("Escriba un número: "))
+
+media = (numero1+numero2+numero3+numero4+numero5) / 5
+
+print("La media aritmética de", numero1, "y", numero2,  "y", numero3,  "y", numero4,  "y", numero5 "es", media)
+
